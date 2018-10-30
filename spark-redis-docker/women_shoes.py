a64ee021262127abdab0d62f61371c699c24fde0
root <root@gray>
2018-10-30 10:11:26 +0000

    first commit

---
+++ spark-redis-docker/women_shoes.py
@@ -0,0 +1,12 @@
+import sys
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext
+
+if __name__ == "__main__":
+
+  conf = SparkConf().setAppName("redis-df").setMaster("local[*]").set("spark.redis.host", "localhost").set("spark.redis.port", "6379")
+  sc = SparkContext(conf=conf)
+  sqlContext = SQLContext(sc)
+  df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("mode", "DROPMALFORMED").option("inferSchema", "true").option("delimiter", ",").load("file:///home/vagrant/7210_1.csv")
+  df_filter = df.select('id','brand','colors','dateAdded')
+  df_filter.write.format("org.apache.spark.sql.redis").option("table", "women_shoesss").option("key.column", "id").save()
